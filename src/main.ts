fb384724a8db2111278d478c956b5afa38cd0606
rorn <rysarorn8@gmail.com>
2022-10-04 21:56:11 +0700

    changePort

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function initApp() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("/api/v1")
-  // await app.listen(process.env.PORT || 5000);
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 5000);
+  // await app.listen(process.env.PORT);
 }
 initApp();
