e841d47bcce86cbaa7656216c8e6a16b8ca1a7e9
rorn <rysarorn8@gmail.com>
2022-10-25 15:02:04 +0700

    changeTest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './Product/product.module';
 import { PrismaModule } from './Prisma/prisma.module';
 import { UserModule } from './User/user.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './Auth/auth.module';
 
 @Module({
   imports: [PrismaModule , ProductModule, UserModule, AuthModule],
