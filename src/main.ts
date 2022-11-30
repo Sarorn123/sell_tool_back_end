3bcfed87dcefce664a0f6156be16621780148755
rorn <rysarorn8@gmail.com>
2022-11-30 19:34:56 +0700

    jj

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import * as cookieParser from 'cookie-parser';
 ////////////////////////////////////////////////
 
 const corsOptions ={
-  origin:'http://localhost:3000', 
+  origin:'*', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
